b75f3479e0ec189a96f4b2e504f62619f54decb0
Kirek <kirek_priest@mail.ru>
2025-04-07 21:17:46 +0500

    small fix

--- src/main/java/ru/gb/springdemo/api/BookController.java
+++ src/main/java/ru/gb/springdemo/api/BookController.java
@@ -1,6 +1,7 @@
 package ru.gb.springdemo.api;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.springdemo.model.Book;
 import ru.gb.springdemo.repository.BookRepository;
@@ -10,14 +11,13 @@ import java.util.List;
 @RestController
 @RequestMapping ("/book")
 public class BookController {
-    //@Autowired
+    @Autowired
     BookRepository bookRepository;
 
     @GetMapping
     public List<Book> getAllBooks(){
         return bookRepository.getAllBooks();
     }
-
 
     @PostMapping
     public void addBook (@RequestBody Book book){
